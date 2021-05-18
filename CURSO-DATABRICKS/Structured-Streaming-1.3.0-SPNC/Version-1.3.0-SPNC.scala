3e59748cdda4e817d3b7e475ca3eacc6c1d0ac92
beto.chong@gmail.com <beto.chong@gmail.com>
2021-05-18 20:44:06 +0000

    ok

---
+++ CURSO-DATABRICKS/Structured-Streaming-1.3.0-SPNC/Version-1.3.0-SPNC.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC 
+// MAGIC %md # Course: Structured-Streaming
+// MAGIC * Version 1.3.0-SPNC
+// MAGIC * Built 2020-01-28 21:59:58 UTC
+// MAGIC * Git revision: b24d6642e389ee336f40bc24fd955b46640e8ae6
+// MAGIC 
+// MAGIC Copyright © 2020 Databricks, Inc.
